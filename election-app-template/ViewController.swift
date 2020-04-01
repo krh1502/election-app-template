43056b616f3a6445ea936f1c9cd7b2e42c8c6bc8
Kate Halushka <khalushka@mindgrub.com>
2020-04-01 10:26:05 -0400

    Initial Commit

---
+++ election-app-template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  election-app-template
+//
+//  Created by Kate Halushka on 4/1/20.
+//  Copyright © 2020 Kate Halushka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
